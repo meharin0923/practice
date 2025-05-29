a650228e7ae5a0e3a03a41a3c9f2c5109c85dbd8
meharin0923 <22d21a05p4meharin@gmail.com>
2025-05-29 18:06:05 +0530

    Add files via upload

--- powerset.py
+++ powerset.py
@@ -1,3 +1,23 @@
-x=int(input())
+'''
+def power(x,n):
+    if(n==0):
+        return
+    if(n%2==1):
+        return x*power(x,n-1)
+    return power(x*n,n//2)
+def getpower(x,n):
+    if(n<0):
+        x=i/x
+        n=abs(n)
+    return getpower(x,n)
 n=int(input())
-print(x**n)
+print(power(x,n))    
+'''
+#another method
+def pow(n):
+    for i in range(31):
+        if(2**i==n):
+            return True
+    return False
+n=int(input())
+print(pow(n))    

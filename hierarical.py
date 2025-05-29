063cd310847c4fb6214657b21722e8bdeabe076a
meharin0923 <22d21a05p4meharin@gmail.com>
2025-05-29 18:00:23 +0530

    Add files via upload

---
+++ hierarical.py
@@ -0,0 +1,13 @@
+class father():
+    def fun1(self):
+        print("i can do project")
+class child1(father):
+    def fun2(self):
+        print("i can play")
+class child2(father):
+    def fun3(self):
+        print("i can eat")
+ob=child1()
+ob.fun1()
+ob.fun2()        
+
